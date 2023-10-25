9d8fd9248f6422217d7f5ecc6a071c4e541434ef
Eduard David <eduarddavid@Eduards-MacBook-Pro.local>
2023-10-25 19:58:45 +0300

    project setup

---
+++ src/test/java/com/example/interViewRbackend/InterViewRBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interViewRbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterViewRBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
