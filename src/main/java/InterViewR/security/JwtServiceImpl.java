d0edb67cf366eb785d09e09effa024fe68483487
Dragos Ciota <dragosciota2002@gmail.com>
2024-01-17 22:07:52 +0200

    comit timp

--- src/main/java/InterViewR/security/JwtServiceImpl.java
+++ src/main/java/InterViewR/security/JwtServiceImpl.java
@@ -46,7 +46,7 @@ public class JwtServiceImpl implements JwtService {
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 10000000 * 60 * 24))
                 .signWith(SignatureAlgorithm.HS256, getSigningKey()).compact();
     }
 
